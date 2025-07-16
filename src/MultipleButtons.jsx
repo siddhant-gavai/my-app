8420755b61252186d625f23b0a344565edcd69b8
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-07-16 23:31:55 +0530

    use fragments

---
+++ src/MultipleButtons.jsx
@@ -0,0 +1,12 @@
+const MultipleButtons = () => {
+  return (
+    <>
+      <h2>Choose an action</h2>
+      <button>Like</button>
+      <button>Share</button>
+      <button>Comment</button>
+    </>
+  );
+};
+
+export default MultipleButtons;
---
+++ src/Title.jsx
@@ -0,0 +1,11 @@
+function Title() {
+  return (
+    <>
+      <h2>Learning React</h2>
+      <button>Click Me</button>
+      <button> This is the button </button>
+    </>
+  );
+}
+
+export default Title;
